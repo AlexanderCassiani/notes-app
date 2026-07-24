64ad8317ebe5eda9ce09890b65a9fba318a566c5
AlexanderCassiani <alexandercassiani74@gmail.com>
2026-07-23 20:22:07 -0500

    agrega el componente Header con estilos y botones de inicio de sesión y creación de cuenta

--- frontend/src/pages/home/header/Header.jsx
+++ frontend/src/pages/home/header/Header.jsx
@@ -1,10 +1,16 @@
+import "./header.css";
+import Button from "../../../components/button/Button";
+
 const Header = () => {
   return (
     <header>
-      <div>
+      <div className="logo">
         <h2>Notely</h2>
       </div>
-      <div></div>
+      <div className="btn-login-options">
+        <Button text="Iniciar sesion" className="btn-login" />
+        <Button text="Crear cuenta" className="btn-signup" />
+      </div>
     </header>
   );
 };
