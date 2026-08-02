4d88919b6b54696a634075adc49c8ba87f388735
AlexanderCassiani <alexandercassiani74@gmail.com>
2026-08-02 15:55:10 -0500

    agrega el servicio de autenticación para el registro de usuarios

---
+++ frontend/src/pages/auth/services/authService.js
@@ -0,0 +1,20 @@
+const API = "http://localhost:8080/api";
+
+export async function signup(name, email, password) {
+  try {
+    const response = await fetch(`${API}/users/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    });
+
+    const data = await response.json();
+
+    return data;
+  } catch (err) {
+    console.error("Ha ocurrido un error al registrarse: ", err);
+    throw err;
+  }
+}
