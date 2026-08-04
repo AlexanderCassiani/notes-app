5be18f0112389626835c673a6576c3e6aca6f4a6
AlexanderCassiani <alexandercassiani74@gmail.com>
2026-08-04 15:24:26 -0500

    agrega función de inicio de sesión con Google

--- backend/feature/auth/auth.controller.js
+++ backend/feature/auth/auth.controller.js
@@ -1,7 +1,10 @@
 import validateUser from "./auth.validation.js";
-import { findByEmail, createUser } from "./auth.model.js";
+import { findByEmail, createUser, createUserGoogle } from "./auth.model.js";
 import bcrypt from "bcrypt";
 import generateToken from "./generateToken.js";
+import { OAuth2Client } from "google-auth-library";
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 export async function signup(req, res) {
   try {
@@ -74,7 +77,7 @@ export async function login(req, res) {
   try {
     user = await findByEmail(email);
   } catch (err) {
-    res.status(500).json({
+    return res.status(500).json({
       success: false,
       message: "Error al registrar el usuario",
     });
@@ -99,7 +102,7 @@ export async function login(req, res) {
   }
 
   if (coincide) {
-    const token = generateToken(user.id, user.email);
+    const token = generateToken(user.id_user, user.email);
     res.json({
       success: true,
       message: "Usuario logueado exitosamente",
@@ -113,6 +116,79 @@ export async function login(req, res) {
     return res.status(401).json({
       success: false,
       message: "Credenciales incorrectas",
+    });
+  }
+}
+
+export async function googleLogin(req, res) {
+  const { credential } = req.body;
+
+  if (!credential) {
+    return res.status(400).json({
+      success: false,
+      message: "No se proporcionó las credenciales",
+    });
+  }
+
+  let payload;
+
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: credential,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    payload = ticket.getPayload();
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      message: "Token de google invalido",
+    });
+  }
+
+  if (!payload.email_verified) {
+    return res.status(401).json({
+      success: false,
+      message: "El correo de Google no está verificado.",
+    });
+  }
+
+  const { name, email } = payload;
+  try {
+    const user = await findByEmail(email);
+
+    if (user) {
+      // si existe el usuario, generar el token
+      const token = generateToken(user.id_user, email);
+
+      return res.json({
+        success: true,
+        message: "Usuario logueado con google exitosamente",
+        user: {
+          email,
+          token,
+        },
+      });
+    }
+
+    // si no existe el usuario, crearlo
+    const newUser = await createUserGoogle(name, email);
+
+    const token = generateToken(newUser.insertId, email);
+
+    return res.json({
+      success: true,
+      message: "Usuario logueado con google exitosamente",
+      user: {
+        id: user.insertId,
+        email,
+        token,
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Ha ocurrido un error al registrarse con google",
     });
   }
 }
