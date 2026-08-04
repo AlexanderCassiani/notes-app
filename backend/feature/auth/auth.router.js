e0b16e9690e7123c1c93d3b077f043c3d30df7bc
AlexanderCassiani <alexandercassiani74@gmail.com>
2026-08-03 20:32:45 -0500

    agrega middleware de limitación de solicitudes a las rutas de autenticación

--- backend/feature/auth/auth.router.js
+++ backend/feature/auth/auth.router.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 import { signup, login } from "./auth.controller.js";
+import authLimit from "../../middleware/rateLimit.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/users/signup", signup);
-authRouter.post("/users/login", login);
+authRouter.post("/users/signup", authLimit, signup);
+authRouter.post("/users/login", authLimit, login);
 
 export default authRouter;
