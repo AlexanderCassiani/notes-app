9903cba17963889edae54479f23b1688f9faeefb
AlexanderCassiani <alexandercassiani74@gmail.com>
2026-08-10 20:44:58 -0500

    agrega la ruta para el componente NoteLayout en AppRoutes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeLayout from "../pages/home/layout/HomeLayout";
 import SignupLayout from "../pages/auth/layout/SignupLayout";
 import LoginLayout from "../pages/auth/layout/LoginLayout";
+import NoteLayout from "../pages/notes/layout/NoteLayout";
 
 import App from "../App";
 
@@ -13,6 +14,7 @@ const AppRoutes = () => {
         <Route path="/" element={<HomeLayout />} />
         <Route path="/signup" element={<SignupLayout />} />
         <Route path="/login" element={<LoginLayout />} />
+        <Route path="/notes" element={<NoteLayout />} />
       </Routes>
     </BrowserRouter>
   );
